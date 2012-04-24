16ab3baecc47606121a28c726d02814097ae4ae4
Christophe VG <xtof@astroboy.local>
2012-04-24 22:06:23 +0200

    added oef87 on Matlab UI development :-(

---
+++ kul/08/oef87.m
@@ -0,0 +1,155 @@
+function varargout = oef87(varargin)
+% oef87 M-file for oef87.fig
+%      oef87, by itself, creates a new oef87 or raises the existing
+%      singleton*.
+%
+%      H = oef87 returns the handle to a new oef87 or the handle to
+%      the existing singleton*.
+%
+%      oef87('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in oef87.M with the given input arguments.
+%
+%      oef87('Property','Value',...) creates a new oef87 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before oef87_OpeningFunction gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to oef87_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help oef87
+
+% Copyright 2001-2003 The MathWorks, Inc.
+
+% Last Modified by GUIDE v2.5 24-Apr-2012 22:04:12
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @oef87_OpeningFcn, ...
+                   'gui_OutputFcn',  @oef87_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin & isstr(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before oef87 is made visible.
+function oef87_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to oef87 (see VARARGIN)
+% Create the data to plot
+handles.peaks=peaks(35);
+handles.membrane=membrane;
+[x,y] = meshgrid(-8:.5:8);
+r = sqrt(x.^2+y.^2) + eps;
+sinc = sin(r)./r;
+handles.sinc = sinc;
+handles.current_data = handles.peaks;
+surf(handles.current_data)
+
+% Choose default command line output for oef87
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes oef87 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = oef87_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function surf_pushbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Display surf plot of the currently selected data
+surf(handles.current_data);
+
+% --- Executes on button press in pushbutton2.
+function mesh_pushbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Display mesh plot of the currently selected data
+mesh(handles.current_data);
+
+% --- Executes on button press in pushbutton3.
+function contour_pushbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Display contour plot of the currently selected data
+contour(handles.current_data);
+
+% --- Executes on selection change in plot_popup.
+function plot_popup_Callback(hObject, eventdata, handles)
+% hObject    handle to plot_popup (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+val = get(hObject,'Value');
+str = get(hObject, 'String');
+switch str{val};
+case 'peaks' % User selects peaks
+	handles.current_data = handles.peaks;
+case 'membrane' % User selects membrane
+	handles.current_data = handles.membrane;
+case 'sinc' % User selects sinc
+	handles.current_data = handles.sinc;
+end
+guidata(hObject,handles)
+mesh(handles.current_data);
+% Hints: contents = get(hObject,'String') returns plot_popup contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from plot_popup
+
+
+% --- Executes during object creation, after setting all properties.
+function plot_popup_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to plot_popup (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc
+    set(hObject,'BackgroundColor','white');
+else
+    set(hObject,'BackgroundColor',get(0,'defaultUicontrolBackgroundColor'));
+end
+
+
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: delete(hObject) closes the figure
+delete(hObject);
